1bb21c78d0485df28a313cdb710d7020376fb36b
Meri Engel <merilengel@gmail.com>
2018-06-30 17:54:46 -0500

    Chapter 4 practice

---
+++ chapter4/ch_4_practice.py
@@ -0,0 +1,41 @@
+"""
+	Complete all of the TODO directions
+	Your file should compile error free
+	Submit your completed file
+"""
+# TODO 4.2 A condition controlled loop
+# write a loop that will change the variable in num by subtracting 1
+# then print the variable. Keep looping until the num = 0 (While num > 0)
+
+
+num = 10
+# write your code here, the variable needs to exist before
+# you test it
+
+# TODO 4.3 the For Loop
+# Use a for loop with a list of the days of the week, print each day
+# of the week
+
+# TODO 4.3 the for loop - range function
+# use the range function to print the numbers from 1 to 10
+
+# TODO 4.4 a running total
+# Have the user enter 5 numbers, provide a total at the end
+# You can assume integers
+
+
+# TODO 4.5 Sentinel Value
+# Create a variable to store a total amount
+# Create a variable to store a count of the numbers entered
+# Have the user enter test scores until a sentinel value of -1 is
+# entered.
+# Display the total, the count and the average (total / count)
+
+
+# TODO 4.6 validating data
+# Ask the user to enter a number between 1 and 10.
+# Use a while loop to force the user to repeat the
+# prompt until the user enters a valid number. Test with
+# both valid and invalid data
+
+
