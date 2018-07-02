769357799e95140669aa278d7b09a518671d8c2a
Meri Engel <mengel7@mchenry.edu>
2018-07-02 12:15:14 -0500

    fixed several confusing directions

--- chapter2/ch_2_practice.py
+++ chapter2/ch_2_practice.py
@@ -1,5 +1,7 @@
 """
     Complete all of the TODO directions
+    The number next to the TODO represents the chapter
+    and section that explain the required code
     Your file should compile error free
     Submit your completed file
 """
@@ -25,7 +27,7 @@
 
 
 # TODO 2.6 keyboard input
-#   Get the user to enter their name and assign it to the variable name
+#   Get the user to enter their name using an input statement and assign it to the variable name
 #   Print a line that greets the user by name (Hello, Meri)
 
 
@@ -35,6 +37,8 @@
 #   when using a comma in a print statement, you can mix numbers and strings
 #   Add 1 to the age (age = age + 1)
 #   Print "Next year you will be ", age
+#   Remember - when printing a variable that holds numbers you need to convert it to a string
+#   str(variable)
 
 
 # TODO 2.7 performing calculations
@@ -43,13 +47,24 @@
 
 
 # TODO 2.7 data conversion
+#   You will write equations as described below, the addition equation is done as a sample
+#   You don't need to assign it to a variable
+
+#   Sample:
+#   Write an equation that adds an integer to an integer, display the equation and the result with a print statement
+print("2 + 2 = " + str(2 + 2))
+
 #   Write an equation that divides an integer by an integer, display the equation and the result with a print statement
+
+
 #   Write an equation that divides an float by a float, display the equation and the result with a print statement
+
 #   Write an equation that divides an float by an integer, display the equation and the result with a print statement
 
 
 # TODO 2.8 Output
 #   modify the following code to print on one line, without merging the lines of code. Separate the words with a hyphen
+#   DO NOT MERGE INTO ONE LINE OF CODE, use escape characters
 print('one')
 print('two')
 print('three')
@@ -59,7 +74,7 @@ print('three')
 print("Sunday Monday Tuesday Wednesday Thursday Friday Saturday")
 
 
-# TODO 2.8 Concatenating strings
+# TODO 2.8 Concatenating strings (Displaying Multiple Items with the + Operator)
 #   Have the user enter their name
 #   Greet the user, concatenate hello and their name into one string
 
@@ -76,5 +91,8 @@ print(number)
 
 # TODO 2.8 Formatting percentage
 #   Format the following as a percentage with 2 as the precision
+#   Sample:
+#       percent = .25834
+#       print(format(percent, '%'))
 percentage = .7654
 print(percentage)
--- chapter4/ch_4_practice.py
+++ chapter4/ch_4_practice.py
@@ -1,5 +1,7 @@
 """
 	Complete all of the TODO directions
+	The number next to the TODO represents the chapter
+    and section that explain the required code
 	Your file should compile error free
 	Submit your completed file
 """
