5c2c8e824c28c68384b2481c5e37ae562c3fa81f
Meri Engel <mengel7@mchenry.edu>
2018-07-02 13:37:55 -0500

    Updated directions, added directories
    Updated the directions and added the remaining files and directories. 6.1 is completed.

---
+++ chapter6/ch_6_practice.py
@@ -0,0 +1,45 @@
+"""
+    Complete all of the TODO directions
+    The number next to the TODO represents the chapter
+    and section that explain the required code
+    Your file should compile error free
+    Submit your completed file
+"""
+
+# TODO 6.1 Introduction to File Input and Output
+# Assign the variable file_variable to open states.txt in read mode
+
+file_variable = open("states.txt", 'r')
+
+# Close the file
+
+file_variable.close()
+
+# Assign the variable state_capitals to open capitals.txt in write mode.
+# Please note, the file does not currently exist, and by opening it in
+# write mode you will create it
+
+
+# Write three state capitals to the file
+# There is a list of state capitals here: https://en.wikipedia.org/wiki/List_of_capitals_in_the_United_States
+# sample
+#   state_capitals.write("Alabama, Montgomery\n") - make sure to include the \n as a new line symbol
+
+
+# close the file
+
+
+# TODO reading data in from a file
+# Assign the variable states_data to open states.txt in read mode
+
+# read in three lines from the file, assign to the variables below
+ line1 =
+ line2 =
+ line3 =
+ 
+# close the file
+
+#print the three variables
+
+# TODO 6.2 Using loops to process files
+
