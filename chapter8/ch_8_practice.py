3cf3a375b47ae6614714c50e6a37d1bf1521727d
Meri Engel <mengel7@mchenry.edu>
2018-07-09 12:21:06 -0500

    chapter 8

--- chapter8/ch_8_practice.py
+++ chapter8/ch_8_practice.py
@@ -5,3 +5,39 @@
     Your file should compile error free
     Submit your completed file
 """
+
+# TODO 8.1 Basic string Operation
+# Print all the characters from the name variable by accessing one character at a time
+
+name = "John Jacob Jingleheimer Schmidt"
+
+# Use the index to access and print the capital s in Schmidt from the variable name
+
+# Use the index with negative numbers to print the letters from the last name "Schmidt" in the
+# name variable
+
+# TODO 8.2 String slicing
+# use string slicing to assign the middle name "Jacob" from name to the variable middle, replace the ""
+
+middle = ""
+
+
+# TODO 8.3 Testing, Searching, and manipulating strings
+# test to see if the string "Jacob" is in name
+
+# test to see if the string "Michael" is in name
+
+# Test to see if name is a number
+
+# Test to see if number is a number
+
+number = 42
+
+# Search for "J" in name, replace with "j" (lower case)
+
+# split the string name into the variable name_list, replace the ""
+name_list = ""
+
+
+
+
