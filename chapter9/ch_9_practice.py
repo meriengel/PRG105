422594d57a01383036dfa8f4c621ec35df998130
Meri Engel <merilengel@gmail.com>
2018-07-14 17:11:24 -0500

    Chapter 4 practice

--- chapter9/ch_9_practice.py
+++ chapter9/ch_9_practice.py
@@ -5,3 +5,58 @@
     Your file should compile error free
     Submit your completed file
 """
+# TODO 9.1 Dictionaries
+# Finish creating the following dictionary by adding three more people and birthdays
+birthdays = {'Meri': 'May 16', 'Kathy': 'July 14'}
+
+# Print Meri's Birthday
+
+# Create an empty dictionary named registration
+
+# You will use the following dictionary for many of the remaining exercises
+
+miles_ridden = {'June 1': 25, 'June 2': 20, 'June 3': 38, 'June 4': 12, 'June 5': 30, 'June 7': 25}
+
+# print the keys and the values of miles_ridden using a for loop
+
+# get the value for June 3 and print it, if not found display 'Entry not found', replace the ""
+
+value = ""
+print(value)
+# get the value for June 6 and print it, if not found display 'Entry not found' replace the ""
+value2 = ""
+print(value2)
+
+# Use the items method to print the miles_ridden dictionary
+
+
+# Use the keys method to print all of the keys in miles_ridden
+
+# Use the pop method to remove june 4 then print the contents of the dictionary
+
+# Use the values method to print the contents of the miles_ridden dictionary
+
+# TODO 9.2 Sets
+# Create an empty set named my_set
+
+# Create a set named days that contains the days of the week
+
+
+# get the number of elements from the days set and print it
+
+# Remove Saturday and Sunday from the days set
+
+# Determine if 'Mon' is in the days set
+
+# TODO 9.3 Serializing Objects (Pickling)
+
+# import the pickle library
+
+# create the output file log and open it for binary writing
+
+# pickle the miles_ridden dictionary and output it to the log file
+
+# close the log file
+
+
+
