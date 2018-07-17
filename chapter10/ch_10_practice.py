f9aa6c0ccb080fff4837149ba401218b23629630
Meri Engel - Programming Instructor McHenry County College <34421299+meriengel@users.noreply.github.com>
2018-07-17 12:48:45 -0500

    coin

--- chapter10/ch_10_practice.py
+++ chapter10/ch_10_practice.py
@@ -4,4 +4,38 @@
     and section that explain the required code
     Your file should compile error free
     Submit your completed file
+    
+    We are going to convert the coin class to a dice class
+    The following code is based on the coin class from your book
 """
+
+import random
+
+
+class Coin:  # note class names are capitalized
+    def __init__(self):
+        self.side_up = 'Heads'
+        
+    def toss(self):
+        if random.randint(0, 1) == 0:
+            self.side_up = 'Heads'
+        else:
+            self.side_up = 'Tails'
+            
+    def get_side_up(self):
+        return self.side_up
+    
+
+def main():
+    my_coin = Coin()
+    my_coin_two = Coin()
+    print('This side is up, ', my_coin.get_side_up())
+    print('This side is up, ', my_coin_two.get_side_up())
+    
+    print('I am tossing the coins...')
+    my_coin.toss()
+    my_coin_two.toss()
+    print('This side is up, ', my_coin.get_side_up())
+    print('This side is up, ', my_coin_two.get_side_up())
+
+main()
