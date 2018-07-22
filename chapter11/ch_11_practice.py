0fdb88504ae99282ee6dab926cc44dd3e2238c72
Meri Engel <merilengel@gmail.com>
2018-07-21 20:49:02 -0500

    Chapter 13 practice

--- chapter11/ch_11_practice.py
+++ chapter11/ch_11_practice.py
@@ -5,3 +5,53 @@
     Your file should compile error free
     Submit your completed file
 """
+
+# TODO 11.1 Introduction to Inheritance
+# You are going to create a Dwelling class based on the Automobile
+# Sample from the chapter
+
+
+# create the class Dwelling, the __init__ method should accept number_of_rooms, square_feet, floors
+
+
+# add the mutators for all of the data attributes (number_of_rooms, square_feet, floors)
+
+
+# add the accessors for all of  the data attributes
+
+
+# Create the class Single_family_home as a sub class of Dwelling
+# The __init__ method should accept number_of_rooms, square_feet, floors, garage_type, yard_size
+
+# call the superclass of Dwelling and pass the required arguments, remember to include self
+
+# initialize the garage_type and yard_size attributes
+
+# create the mutator and accessor methods for the garage_type and yard_size attributes
+
+
+# demonstrate the Single_family_home class, no need to import because you are in the same file
+
+# create an object from the Single_family_home class with the following information:
+# 6 rooms, 1200 square feet, 1 floor, single car garage, .25 acres
+# Display the data using the accessor methods
+
+
+# create the main function
+
+# call the main function
+
+
+# TODO 11.2 Polymorphism
+# Type in the mammal class from program 11-9    lines 1 - 22
+
+
+# create a Mouse class as a sub class of the mammal class following the Dog example
+
+# create a Bird class as a sub class of the mammal class following the Cat Example
+
+
+# Follow the example in program 11-10 (no need to import, use main2 instead of main
+# because there is already a main on this page) use the Mouse and Bird class that you created
+
+
---
+++ chapter13/ch_13_practice.py
@@ -0,0 +1,41 @@
+"""
+    Complete all of the TODO directions
+    The number next to the TODO represents the chapter
+    and section that explain the required code
+    Your file should compile error free
+    Submit your completed file
+"""
+import tkinter
+
+# TODO 13.2 Using the tkinter Module
+
+# Write the code from program 13-2 to display an empty window, no need
+# to re-import tkinter
+
+# create the class MyGUI2
+
+# TODO 13.3 Adding a label widget
+# add a label that prints your first and last name
+
+# pack the label
+
+# enter the main loop
+
+# create an instance of MyGUI2
+
+# TODO 13.4 Organizing Widgets with Frames
+# Create a window in the MyGUI3 class that has two frames
+# In the top Frame add a labels with your name and major
+# In the bottom frame add labels with the classes you are taking this semester
+
+
+# TODO 13.5 Button Widgets and info Dialog Boxes
+# Tell a joke with a button to show the punch line, which should appear in a dialog box
+
+
+# TODO 13.6 getting input / 13.7 Using Labels as output fields
+# Using the program in 13.10 kilo converter as a sample, create a program
+# to convert inches to centimeters
+
+
+
