0b948a32e9bb697696b7b96faf3b7bed28f2e5c6
Meri Engel - Programming Instructor McHenry County College <34421299+meriengel@users.noreply.github.com>
2018-09-18 13:44:16 -0500

    import math

--- chapter5/ch_5_practice.py
+++ chapter5/ch_5_practice.py
@@ -119,7 +119,7 @@ main6()
 
 """
 # TODO 5.9 the math module
-# import math
+# import math - add the import math statement at the top of this file
 # write a statement that uses the ceil function on the following variable
 
 number_to_round = 4.243
