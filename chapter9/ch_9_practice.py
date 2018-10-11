2c86edc38d71e9d4acc2191c4c85a19edc0a30b0
Steffen Hayes <31227039+shaypatricks@users.noreply.github.com>
2018-10-11 10:41:47 -0500

    This is my way to organize our work

--- chapter9/ch_9_practice.py
+++ chapter9/ch_9_practice.py
@@ -5,58 +5,67 @@
     Your file should compile error free
     Submit your completed file
 """
-# TODO 9.1 Dictionaries
-# Finish creating the following dictionary by adding three more people and birthdays
+print("TODO 9.1 Dictionaries")
+print("\n---------------------- 9.1 part 1----------------------------------------------")
+print("1.Finish creating the following dictionary by adding three more people and birthdays")
 birthdays = {'Meri': 'May 16', 'Kathy': 'July 14'}
 
-# 1.Print Meri's Birthday
+print("\n 2.Print Meri Birthday")
 
-# 2.Create an empty dictionary named registration
+print("\n 3.Create an empty dictionary named registration")
 
-# 3.You will use the following dictionary for many of the remaining exercises
 
+print("\nYou will use the following dictionary for many of the remaining exercises")
+print("\n---------------------- 9.1 part 2----------------------------------------------")
 miles_ridden = {'June 1': 25, 'June 2': 20, 'June 3': 38, 'June 4': 12, 'June 5': 30, 'June 7': 25}
 
-# print the keys and the values of miles_ridden using a for loop
+print("1.print the keys and the values of miles_ridden using a for loop")
 
-# get the value for June 3 and print it, if not found display 'Entry not found', replace the ""
+
+print("\n")
+print("2.get the value for June 3 and print it, if not found display 'Entry not found', replace the """)
 
 value = ""
 print(value)
-# get the value for June 6 and print it, if not found display 'Entry not found' replace the ""
+
+print("\n")
+print("3.get the value for June 6 and print it, if not found display 'Entry not found' replace the """)
 value2 = ""
 print(value2)
+print("\n")
 
-# Use the items method to print the miles_ridden dictionary
-
-
-# Use the keys method to print all of the keys in miles_ridden
-
-# Use the pop method to remove june 4 then print the contents of the dictionary
-
-# Use the values method to print the contents of the miles_ridden dictionary
-
-# TODO 9.2 Sets
-# Create an empty set named my_set
-
-# Create a set named days that contains the days of the week
+print("4.Use the items method to print the miles_ridden dictionary")
 
 
-# get the number of elements from the days set and print it
+print("\n5.Use the keys method to print all of the keys in miles_ridden")
 
-# Remove Saturday and Sunday from the days set
+print("\n6.Use the pop method to remove june 4 then print the contents of the dictionary")
 
-# Determine if 'Mon' is in the days set
+print("\n7. Use the values method to print the contents of the miles_ridden dictionary")
 
-# TODO 9.3 Serializing Objects (Pickling)
 
-# import the pickle library
+print("\n---------------------- TODO 9.2 Sets----------------------------------------------")
 
-# create the output file log and open it for binary writing
+print("\n 1.Create an empty set named my_set")
 
-# pickle the miles_ridden dictionary and output it to the log file
+print("\n 2.Create a set named days that contains the days of the week")
 
-# close the log file
+
+print("\n 3. get the number of elements from the days set and print it")
+
+print("\n 4. Remove Saturday and Sunday from the days set")
+
+print("\n 5.Determine if 'Mon' is in the days set")
+
+print("\n---------------------- TODO 9.3 Serializing Objects (Pickling)----------------------------------------------")
+
+print("\n 1. import the pickle library at the top of this file")
+
+print("\n 2. create the output file log and open it for binary writing")
+
+print("\n 3. pickle the miles_ridden dictionary and output it to the log file")
+
+print("\n 4. close the log file")
 
 
 
