fbaf085d6c2a3e3e4d1f754d2c8f3e7315e7cae2
Meri Engel - Programming Instructor McHenry County College <34421299+meriengel@users.noreply.github.com>
2018-10-11 09:46:55 -0500

    formatting

--- chapter8/ch_8_practice.py
+++ chapter8/ch_8_practice.py
@@ -7,35 +7,43 @@
 """
 
 # TODO 8.1 Basic string Operation
-# Print all the characters from the name variable by accessing one character at a time
-
+# 1. Print all the characters from the name variable by accessing one character at a time
+print("---------------------------------8.1 question 1 ---------------------------------------")
 name = "John Jacob Jingleheimer Schmidt"
 
-# Use the index to access and print the capital s in Schmidt from the variable name
+# 2. Use the index to access and print the capital s in Schmidt from the variable name
+print("---------------------------------8.1 question 2 ---------------------------------------")
 
-# Use the index with negative numbers to print the letters from the last name "Schmidt" in the
-# name variable
+# 3. Use the index with negative numbers to print the letters from the last name "Schmidt" in the
+#    name variable
+print("---------------------------------8.1 question 3 ---------------------------------------")
 
 # TODO 8.2 String slicing
-# use string slicing to assign the middle name "Jacob" from name to the variable middle, replace the ""
-
+# 1. Use string slicing to assign the middle name "Jacob" from name to the variable middle, replace the ""
+# 2. Print middle
+print("---------------------------------8.2 question 1 ---------------------------------------")
 middle = ""
 
 
 # TODO 8.3 Testing, Searching, and manipulating strings
-# test to see if the string "Jacob" is in name
+print("---------------------------------8.3 question 1 ---------------------------------------")
+# 1. Test to see if the string "Jacob" is in name, print the result
 
-# test to see if the string "Michael" is in name
+print("---------------------------------8.3 question 2 ---------------------------------------")
+# 2. Test to see if the string "Michael" is in name, print the result
 
-# Test to see if name is a number
+print("---------------------------------8.3 question 3 ---------------------------------------")
+# 3. Test to see if name is a number, print the result
 
-# Test to see if number is a number
-
+print("---------------------------------8.3 question 4 ---------------------------------------")
+# 4. Test to see if number is a number, print the result
 number = "42"
 
-# Search for "J" in name, replace with "j" (lower case)
+print("---------------------------------8.3 question 5 ---------------------------------------")
+# 5. Search for "J" in name, replace with "j" (lower case), print the result - note either assign this to a variable or just print
 
-# split the string name into the variable name_list, replace the ""
+print("---------------------------------8.3 question 6 ---------------------------------------")
+# 6. Split the string name into the variable name_list, replace the ""
 name_list = ""
 
 
