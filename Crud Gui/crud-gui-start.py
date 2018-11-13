ce8968a7e538b577b22f89ecb8cdcc8f5bf62def
Cindy Grieb <cgrieb@mchenry.edu>
2018-11-12 19:11:16 -0600

    Add files via upload

--- Crud Gui/crud-gui-start.py
+++ Crud Gui/crud-gui-start.py
@@ -3,18 +3,18 @@ import tkinter.messagebox
 import pickle
 
 
+# main (root) GUI menu
 class CrudGUI:
     def __init__(self, master):
         self.master = master
         self.master.title('Welcome Menu')
-        self.radio_var = 0
-        
+
         self.top_frame = tkinter.Frame(self.master)
         self.bottom_frame = tkinter.Frame(self.master)
-        
+
         self.radio_var = tkinter.IntVar()
         self.radio_var.set(1)
-        
+
         # create the radio buttons
         self.look = tkinter.Radiobutton(self.top_frame, text='Look up customer',
                                         variable=self.radio_var, value=1)
@@ -24,25 +24,25 @@ class CrudGUI:
                                           variable=self.radio_var, value=3)
         self.delete = tkinter.Radiobutton(self.top_frame, text='Delete customer',
                                           variable=self.radio_var, value=4)
-        
+
         # pack the radio buttons
         self.look.pack(anchor='w', padx=20)
         self.add.pack(anchor='w', padx=20)
         self.change.pack(anchor='w', padx=20)
         self.delete.pack(anchor='w', padx=20)
-        
+
         # create ok and quit buttons
         self.ok_button = tkinter.Button(self.bottom_frame, text='OK', command=self.open_menu)
         self.quit_button = tkinter.Button(self.bottom_frame, text='QUIT', command=self.master.destroy)
-        
+
         # pack the buttons
         self.ok_button.pack(side='left')
         self.quit_button.pack(side='left')
-        
+
         # pack the frames
         self.top_frame.pack()
         self.bottom_frame.pack()
-    
+
     def open_menu(self):
         if self.radio_var.get() == 1:
             search = LookGUI(self.master)
@@ -52,7 +52,7 @@ class CrudGUI:
 
 class LookGUI:
     def __init__(self, master):
-        
+
         # open the file, load to customers, close file. Do in each class
         try:
             input_file = open("customer_file.dat", 'rb')
@@ -60,51 +60,51 @@ class LookGUI:
             input_file.close()
         except (FileNotFoundError, IOError):
             self.customers = {}
-        
-        self.master = master
+
+        # tkinter.Toplevel() is like tkinter.Frame() but it opens in a new window
         self.look = tkinter.Toplevel(master)
         self.look.title('Search for customer')
-        
-        # self.look_window = tkinter.Tk()
+
+        # create Frames for this Toplevel window
         self.top_frame = tkinter.Frame(self.look)
         self.middle_frame = tkinter.Frame(self.look)
         self.bottom_frame = tkinter.Frame(self.look)
-        
-        # widgets for top frame
+
+        # widgets for top frame - label and entry box for name
         self.search_label = tkinter.Label(self.top_frame, text='Enter customer name to look for: ')
         self.search_entry = tkinter.Entry(self.top_frame, width=15)
-        
+
         # pack top frame
         self.search_label.pack(side='left')
         self.search_entry.pack(side='left')
-        
-        # middle frame
+
+        # middle frame - label for results
         self.value = tkinter.StringVar()
         self.info = tkinter.Label(self.middle_frame, text='Results: ')
         self.result_label = tkinter.Label(self.middle_frame, textvariable=self.value)
-        
+
         # pack Middle frame
         self.info.pack(side='left')
         self.result_label.pack(side='left')
-        
+
         # buttons for bottom frame
         self.search_button = tkinter.Button(self.bottom_frame, text='Search', command=self.search)
         self.back_button = tkinter.Button(self.bottom_frame, text='Main Menu', command=self.back)
-        
+
         # pack bottom frame
         self.search_button.pack(side='left')
         self.back_button.pack(side='left')
-        
+
         # pack frames
         self.top_frame.pack()
         self.middle_frame.pack()
         self.bottom_frame.pack()
-    
+
     def search(self):
         name = self.search_entry.get()
         result = self.customers.get(name, 'Not Found')
         self.value.set(result)
-    
+
     def back(self):
         self.look.destroy()
 
